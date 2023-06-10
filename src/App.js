354209a7c79fc1664269079310dfe81264fd0765
Stathis-Renieris <unified.wised@gmail.com>
2023-06-10 18:12:03 +0300

    Functionality restored

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { lazy, useState, Suspense } from "react";
+
+const Globe = lazy(() => import("./globe"));
+const loadGlobeComponent = () => import("./globe");
 
 function App() {
+  const [showGlobe, setShowGlobe] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+        justifyContent: "center",
+        height: "100%",
+        padding: "2rem",
+      }}
+    >
+      <label
+        style={{ marginBottom: "1rem" }}
+        onFocus={loadGlobeComponent}
+        onMouseEnter={loadGlobeComponent}
+      >
+        <input
+          type="checkbox"
+          checked={showGlobe}
+          onChange={(e) => setShowGlobe(e.target.checked)}
+        />
+        {" show globe"}
+      </label>
+      <div style={{ width: 400, height: 400 }}>
+        <Suspense fallback={<div>Loading...</div>}>
+          {showGlobe ? <Globe /> : null}
+        </Suspense>
+      </div>
     </div>
   );
 }
